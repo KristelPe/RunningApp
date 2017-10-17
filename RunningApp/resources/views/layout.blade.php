0c4c2f8bf6f675c303c5e2d8f86623e2d21713b4
Jolan De Nef <jolandenef@hotmail.com>
2017-10-17 17:10:52 +0200

    css parkour desktop

--- RunningApp/resources/views/layout.blade.php
+++ RunningApp/resources/views/layout.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="../css/@yield('css')">
+    <link rel="stylesheet" href="../css/app.css">
     <title>@yield('title')</title>
 </head>
 <body>
--- RunningApp/resources/views/parkours/index.blade.php
+++ RunningApp/resources/views/parkours/index.blade.php
@@ -1,13 +1,80 @@
 @extends('layout')
 
 @section('title', 'Parkours')
+@section('css', 'parkours.css')
+
 @section('content')
     @parent
 
-    <h1>Parkour - overzicht</h1>
 
-    <ul>
-        <li><a href="/parkour/1">Parkour 1</a></li>
-    </ul>
+
+
+    <div class="parkour_container">
+        <h1>PARKOURS</h1>
+        <hr class="bar">
+
+        <ul class="parkours">
+
+
+{{--
+            @foreach
+--}}
+
+                <li class="parkour_name">
+                    <a href="/parkour/1">   <h2>Vrijbroekpark Mechelen</h2> <p>5,5 km</p>    </a>
+                </li>
+
+                <li>
+
+                    <div class="parkour_counts">
+
+                            <p class="parkour_text">20 people recommend this route</p>
+
+                        <p class="parkour_text">71 people ran this route</p>
+
+                        <div class="parkour_image">
+
+                        </div>
+                    </div>
+
+                </li>
+
+            {{--Recommend this to other runners--}}
+                <li>
+
+                    <div class="btns">
+
+                    <button type="submit" class="btn recommend">
+
+                        Recommend
+
+                    </button>
+            {{--Plan group run -> Announce you are going to run this route to your group members--}}
+
+                    <button type="submit" class="btn plan">
+
+                        Plan group run
+
+                    </button>
+
+                    </div>
+
+                </li>
+
+                <li>
+
+                    <img src="../images/star" alt="star" class="star">
+
+                </li>
+
+
+
+            {{--@endforeach--}}
+
+        </ul>
+
+        <hr class="bar split">
+
+    </div>
 
 @endsection
