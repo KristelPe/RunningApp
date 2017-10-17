7eee75cd76e5517e151e22a824350cad8553d455
Thomas <Thomas.corbeel@gmail.com>
2017-10-17 18:10:08 +0200

    added log out function

--- RunningApp/resources/views/nav.blade.php
+++ RunningApp/resources/views/nav.blade.php
@@ -4,17 +4,29 @@
         <li class="{{ (Request::is('profile*') ? 'active' : '') }}"><a href="/profile">Profile</a></li>
         <li class="{{ (Request::is('parkours*') ? 'active' : '') }}"><a href="/parkours">Parkour</a></li>
         <li class="{{ (Request::is('groups*') ? 'active' : '') }}"><a href="/groups">Groups</a></li>
-        <li>
-            <a href="/login" class="login">
-                Log in
-            </a>
-        </li>
+
+        @if( $loggedIn)
+            <li>
+                <a href="/logout" class="login">
+                    Log out
+                </a>
+            </li>
+        @else
+            <li>
+                <a href="/login" class="login">
+                    Log in
+                </a>
+            </li>
+        @endif
     </ul>
+    @if( $loggedIn )
     <a href="/profile">
-    <div class="user_img">
+    <img src="{{ $userAvatarM }}" class="user_img">
         USER IMG
-    </div>
+    </img>
+
     </a>
+    @endif
 </nav>
 
 
