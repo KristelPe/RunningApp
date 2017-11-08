03452f10d851cea7a725eaf59d84ed0574f64deb
Thomas <Thomas.corbeel@gmail.com>
2017-11-08 08:41:37 +0100

    temp fix for demo

--- RunningApp/app/Http/Controllers/Controller.php
+++ RunningApp/app/Http/Controllers/Controller.php
@@ -20,6 +20,56 @@ class Controller extends BaseController
             $avatarM = Session::get('userAvatarMedium');
             $userName = Session::get('userFirstName');
             $userId = Session::get('userId');
+            $token = Session::get('token');
+
+
+            //ZEER VUIL!!!
+
+            $acts = StravaController::getAllUserActivity($token);
+
+            //uncomment volgende lijn om de json in uw browser te zien
+            //dd($acts);
+
+
+            $totalDistance = 0;
+            $maxSpeed = 0;
+            $longestDistance = 0;
+            foreach ($acts as $actClass){
+                $act = (array)$actClass;
+                $totalDistance = $totalDistance + $act['distance'];
+                $maxSpeed = max($act['max_speed'], $maxSpeed);
+                $longestDistance = max($act['distance'], $longestDistance);
+
+
+                if (Activity::where('id', '=', $act['id'])->exists()) {
+                }else {
+                    if ($act != null) {
+                        $athlete = (array)$act['athlete'];
+
+                        $act['start_date_local'] = preg_replace('/[^0-9.]+/', '', $act['start_date_local']);
+                        $act['start_date_local'] = substr($act['start_date_local'], 0, 8);
+
+
+                        $newActivity = Activity::create([
+                            'id' => $act['id'],
+                            'athlete_id' => $athlete['id'],
+                            'name' => $act['name'],
+                            'distance' => $act['distance'],
+                            'start_date_local' => $act['start_date_local'],
+                            'max_speed' => $act['max_speed'],
+                            'average_speed' => $act['average_speed'],
+                            'type' => $act['type'],
+                            'moving_time' => $act['moving_time'],
+                            'elapsed_time' => $act['elapsed_time'],
+                            'kudos_count' => $act['kudos_count'],
+                        ]);
+
+                        $newActivity->save();
+                    }
+                }
+            }
+
+            //EINDE VUILIGHEID!!!
 
 
             $runDistance = 0;
@@ -27,7 +77,7 @@ class Controller extends BaseController
             $recomendedDistance = 2000;
 
         if(Activity::where('athlete_id', $userId) != Null) {
-            $allActivities = Activity::where('athlete_id', $userId)->where('start_date_local', '>' ,20171101)->get();
+            $allActivities = Activity::where('athlete_id', $userId)->where('start_date_local', '>' ,20171106)->get();
             $x = 1;
             foreach ($allActivities as $a) {
                 $runDistance = $runDistance + $a->distance;
--- RunningApp/resources/views/users/index.blade.php
+++ RunningApp/resources/views/users/index.blade.php
@@ -24,7 +24,7 @@
         <div class="switch_button">
             <ul>
                 <li class="switch_button_1" onclick="switchEffect(1)">Stats</li>
-                <li class="switch_button_2" onclick="switchEffect(2)">Medals</li>
+                <li class="switch_button_2" onclick="switchEffect(2)">Recent Activities</li>
             </ul>
         </div>
 
