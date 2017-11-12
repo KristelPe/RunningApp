d8a4a9f0969e08a1c797c695759a24ecc447aafb
Thomas <Thomas.corbeel@gmail.com>
2017-11-12 10:41:37 +0100

    Schedule class

---
+++ RunningApp/app/Schedule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    use Notifiable;
+
+
+    protected $fillable = [
+        'id','name', 'ownerId', 'endGoal','endGoalIn','endDate'
+    ];
+
+}
