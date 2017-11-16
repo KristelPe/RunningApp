bcd2b91d67511c5a22790c2db75501669b9de2b9
MichielJanssens <michiel.janssens88@gmail.com>
2017-11-16 17:06:06 +0100

    small css fixes for parcour page

--- RunningApp/resources/views/parcours/index.blade.php
+++ RunningApp/resources/views/parcours/index.blade.php
@@ -23,18 +23,22 @@
 
                 @if(Auth::user()->id == $parcour->athlete_id)
 
-                    <div class="parcour_segment">
-                        <img src="" alt="img missing" class="parcour_img">
-                            <div class="parcour_details">
-                                <a href="parcours/{{$parcour->id}}">
-                                    <h2>{{$parcour->name}}</h2>
-                                    <p>Distance: {{$parcour->distance / 1000}} KM</p>
-                                    <p>Runner: {{$parcour->getUser->firstName}} {{$parcour->getUser->lastName}} </p>
-                                </a>
-                            </div>
-                    </div>
+                    <div class="parcour_segment_layout">
 
-                    <hr class="bar">
+                        <div class="parcour_segment">
+                            <img src="" alt="img missing" class="parcour_img">
+                                <div class="parcour_details">
+                                    <a href="parcours/{{$parcour->id}}">
+                                        <h2>{{$parcour->name}}</h2>
+                                        <p>Distance: {{$parcour->distance / 1000}} KM</p>
+                                        <p>Runner: {{$parcour->getUser->firstName}} {{$parcour->getUser->lastName}} </p>
+                                    </a>
+                                </div>
+                        </div>
+
+                        <hr class="bar">
+
+                    </div>
 
                 @endif
 
@@ -50,19 +54,23 @@
 
                 @if(Auth::user()->id != $parcour->athlete_id)
 
-                    <div class="parcour_segment">
-                        <img src="" alt="img missing" class="parcour_img">
-                        <div class="parcour_details">
-                            <a href="parcours/{{$parcour->id}}">
-                                <h2>{{$parcour->name}}</h2>
-                                <p>Distance: {{$parcour->distance / 1000}} KM</p>
-                                <p>Runner: {{$parcour->getUser->firstName}} {{$parcour->getUser->lastName}} </p>
-                                <p>Advice: Perfect for you!</p>
-                            </a>
-                        </div>
-                    </div>
+                    <div class="parcour_segment_layout">
 
-                    <hr class="bar">
+                        <div class="parcour_segment">
+                            <img src="" alt="img missing" class="parcour_img">
+                            <div class="parcour_details">
+                                <a href="parcours/{{$parcour->id}}">
+                                    <h2>{{$parcour->name}}</h2>
+                                    <p>Distance: {{$parcour->distance / 1000}} KM</p>
+                                    <p>Runner: {{$parcour->getUser->firstName}} {{$parcour->getUser->lastName}} </p>
+                                    <p>Advice: Perfect for you!</p>
+                                </a>
+                            </div>
+                        </div>
+
+                        <hr class="bar">
+
+                    </div>
 
                 @endif
 
